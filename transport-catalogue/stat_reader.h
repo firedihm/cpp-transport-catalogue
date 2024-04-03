73afea84d9a00c8a0374bf96f973f7cf4bfdb899
firedihm <42544757+firedihm@users.noreply.github.com>
2024-04-03 13:41:05 +0300

    Update stat_reader.h

--- transport-catalogue/stat_reader.h
+++ transport-catalogue/stat_reader.h
@@ -1,1 +1,12 @@
-// место для вашего кода
+#pragma once
+
+#include "transport_catalogue.h"
+
+#include <iosfwd>
+//#include <string_view>
+
+namespace catalogue::stat {
+
+void ParseAndPrintStat(const TransportCatalogue& catalogue, std::string_view request, std::ostream& output);
+
+} // namespace catalogue::stats
