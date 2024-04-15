7581b83dbf9a684147543778ae9cd759fd64ce40
firedihm <42544757+firedihm@users.noreply.github.com>
2024-04-15 19:01:31 +0300

    Update geo.h

--- transport-catalogue/geo.h
+++ transport-catalogue/geo.h
@@ -4,6 +4,8 @@
 
 namespace catalogue::geo {
 
+const int EARTH_RADIUS = 6371000; // в метрах
+
 struct Coordinates {
     double lat;
     double lng;
@@ -21,10 +23,10 @@ inline double ComputeDistance(Coordinates from, Coordinates to) {
     if (from == to) {
         return 0;
     }
-    static const double dr = 3.1415926535 / 180.;
+    static const double dr = 3.1415926535 / 180.0;
     return acos(sin(from.lat * dr) * sin(to.lat * dr)
                 + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
-        * 6371000;
+        * EARTH_RADIUS;
 }
 
 } // namespace catalogue::geo
