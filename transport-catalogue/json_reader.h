2f74b1829553c26e4326bd90b49a7380f4d3d4a9
firedihm <42544757+firedihm@users.noreply.github.com>
2024-05-10 19:22:09 +0300

    Update json_reader.h

--- transport-catalogue/json_reader.h
+++ transport-catalogue/json_reader.h
@@ -1,11 +1,35 @@
 #pragma once
 
 #include "json.h"
+#include "map_renderer.h"
 #include "transport_catalogue.h"
 
 namespace json {
 
-void ParseAndPrintRequests(catalogue::TransportCatalogue& catalogue, const Document& input, std::ostream& output, int step = 4, int indent = 0);
-void ParseAndRenderMap(catalogue::TransportCatalogue& catalogue, const Document& input, std::ostream& output, int step = 0, int indent = 4);
+class JsonReader {
+public:
+    JsonReader(catalogue::TransportCatalogue& catalogue, const Document& input, std::ostream& output)
+        : catalogue_(catalogue), input_(input), output_(output) {}
+    
+    void ProcessBaseRequests();
+    const Document ProcessStatRequests();
+    void Print(const Document& response, int step = 4, int indent = 0);
+    void RenderMap(int step = 0, int indent = 4);
+    
+private:
+    Dict MakeBusResponse(const Dict& request);
+    Dict MakeStopResponse(const Dict& request);
+    Dict MakeMapResponse(const Dict& request);
+    
+    static geo::Coordinates ParseCoordinates(const Dict& request);
+    static std::vector<std::pair<std::string_view, int>> ParseDistances(const Dict& request);
+    static std::vector<std::string_view> ParseRoute(const Dict& request);
+    static svg::Color ParseColor(const Node& node);
+    static render::RenderDetails ParseRenderDetails(const Dict& settings);
+    
+    catalogue::TransportCatalogue& catalogue_;
+    const Document& input_;
+    std::ostream& output_;
+};
 
 } // namespace json
