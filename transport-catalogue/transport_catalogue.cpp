d2c98217a1fbfafc95728c00d95bcb753bdd742f
firedihm <42544757+firedihm@users.noreply.github.com>
2024-05-10 16:03:48 +0300

    Update transport_catalogue.cpp

--- transport-catalogue/transport_catalogue.cpp
+++ transport-catalogue/transport_catalogue.cpp
@@ -1,45 +1,28 @@
-#include "geo.h"
 #include "transport_catalogue.h"
 
 #include <algorithm>
-#include <stdexcept>
 
 namespace catalogue {
 
 const Stop* TransportCatalogue::GetStop(std::string_view key) const {
-    try {
-        return stops_view_.at(key);
-    } catch (std::out_of_range& e) {
-        throw e;
-    }
+    auto it = stops_view_.find(key);
+    return it != stops_view_.end() ? it->second : nullptr;
 }
 
 const Bus* TransportCatalogue::GetBus(std::string_view key) const {
-    try {
-        return buses_view_.at(key);
-    } catch (std::out_of_range& e) {
-        throw e;
-    }
+    auto it = buses_view_.find(key);
+    return it != buses_view_.end() ? it->second : nullptr;
 }
 
-const std::set<std::string_view>& TransportCatalogue::GetBusesForStop(const Stop* stop) const {
-    try {
-        return stop_to_buses_.at(stop);
-    } catch (std::out_of_range& e) {
-        throw e;
-    }
+const std::set<std::string_view>* TransportCatalogue::GetBusesForStop(const Stop* stop) const {
+    auto it = stop_to_buses_.find(stop);
+    return it != stop_to_buses_.end() ? &(it->second) : nullptr;
 }
 
 int TransportCatalogue::GetDistanceBetweenStops(const Stop* from, const Stop* to) const {
-    try {
-        return distances_.at(std::pair(from, to));
-    } catch (std::out_of_range& e) {
-        try {
-            return distances_.at(std::pair(to, from));
-        } catch (std::out_of_range& e) {
-            return 0;
-        }
-    }
+    auto it = distances_.find(std::pair(from, to));
+    return it != distances_.end() ? it->second
+                                  : (it = distances_.find(std::pair(to, from))) != distances_.end() ? it->second : 0;
 }
 
 void TransportCatalogue::AddStop(const std::string& id, geo::Coordinates&& coords) {
@@ -54,14 +37,29 @@ void TransportCatalogue::AddDistance(const std::string& id, std::vector<std::pai
     }
 }
 
-void TransportCatalogue::AddBus(const std::string& id, std::vector<std::string_view>&& route) {
+void TransportCatalogue::AddBus(const std::string& id, std::vector<std::string_view>&& route, bool is_ring) {
+    // маршрут строится как последовательность указателей на соответствующие названиям из route остановки.
     std::vector<const Stop*> stop_ptrs;
-    stop_ptrs.reserve(route.size());
+    stop_ptrs.reserve(is_ring ? route.size() : 2 * route.size() - 1);
     for (std::string_view stop : route) {
-        stop_ptrs.push_back(GetStop(stop));
+        const Stop* stop_ptr = GetStop(stop);
+        stop_ptrs.push_back(stop_ptr);
+        
+        // обновляем здесь, чтобы при масштабировании не учитывать остановки без автобусов; сначала min...
+        min_max_coords_.first.lat = std::min(min_max_coords_.first.lat, stop_ptr->coords.lat);
+        min_max_coords_.first.lng = std::min(min_max_coords_.first.lng, stop_ptr->coords.lng);
+        // ...потом max
+        min_max_coords_.second.lat = std::max(min_max_coords_.second.lat, stop_ptr->coords.lat);
+        min_max_coords_.second.lng = std::max(min_max_coords_.second.lng, stop_ptr->coords.lng);
+    }
+    // отзеркалим некольцевые маршруты
+    if (!is_ring) {
+        for (auto it = stop_ptrs.rbegin() + 1; it != stop_ptrs.rend(); ++it) {
+            stop_ptrs.push_back(*it);
+        }
     }
     
-    const Bus& ref = buses_.emplace_back(id, std::move(stop_ptrs));
+    const Bus& ref = buses_.emplace_back(id, std::move(stop_ptrs), is_ring ? RouteType::RING : RouteType::PENDULUM);
     buses_view_.emplace(ref.name, &ref);
     
     for (const Stop* stop : ref.route) {
@@ -75,7 +73,7 @@ int TransportCatalogue::CountUniqueStops(const Bus* bus) {
     
     // считать будем только количество разных по значению соседних элементов
     int unique = 0;
-    for (int i = 0; i < sorted.size(); ++i) {
+    for (uint i = 0; i < sorted.size(); ++i) {
         // пока соседние элементы равны пропускаем итерацию
         while (i < sorted.size() - 1 && sorted[i] == sorted[i + 1]) {
             ++i;
