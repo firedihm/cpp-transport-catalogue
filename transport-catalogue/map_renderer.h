37a2d0f05214c3beb4fd039eaaa85033c3e4fd84
firedihm <42544757+firedihm@users.noreply.github.com>
2024-05-10 21:22:02 +0300

    Update map_renderer.h

--- transport-catalogue/map_renderer.h
+++ transport-catalogue/map_renderer.h
@@ -25,6 +25,46 @@ struct RenderDetails {
     std::vector<svg::Color> colors;
 };
 
+class MapRenderer {
+public:
+    MapRenderer(RenderDetails&& details, const catalogue::TransportCatalogue& catalogue, std::ostream& output)
+        : details_(std::move(details)), catalogue_(catalogue), output_(output) {
+        
+        InitScalingFactors();
+        InitSortedLists();
+        RenderMap(); // можно просто рисовать отсюда
+    }
+    MapRenderer(const MapRenderer&) = delete;
+    MapRenderer(MapRenderer&&) = delete;
+    
+    MapRenderer& operator=(const MapRenderer&) = delete;
+    MapRenderer& operator=(MapRenderer&&) = delete;
+    
+private:
+    void InitScalingFactors();
+    void InitSortedLists();
+    
+    void RenderMap();
+    void DrawBusTraces();
+    void DrawBusNames();
+    void DrawStops();
+    void DrawStopNames();
+    
+    svg::Point TransformCoordsToScreenSpace(const geo::Coordinates& coords);
+    
+    const RenderDetails details_;
+    const catalogue::TransportCatalogue& catalogue_;
+    svg::Document document_;
+    std::ostream& output_;
+    
+    double min_lng_;
+    double max_lat_;
+    double zoom_;
+    
+    std::vector<const catalogue::Stop*> sorted_stops_;
+    std::vector<const catalogue::Bus*> sorted_buses_;
+};
+
 void RenderMap(RenderDetails&& details, const catalogue::TransportCatalogue& catalogue, std::ostream& output, int step = 0, int indent = 4);
 
 } // namespace render
