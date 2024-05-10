7a8248cea3af14cb0554c0791109dedd7d7e0468
firedihm <42544757+firedihm@users.noreply.github.com>
2024-05-10 16:04:50 +0300

    Create json.h

---
+++ transport-catalogue/json.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <map>
+#include <stdexcept>
+#include <string>
+#include <variant>
+#include <vector>
+
+namespace json {
+
+class Node;
+using Array = std::vector<Node>;
+using Dict = std::map<std::string, Node>;
+
+// Эта ошибка должна выбрасываться при ошибках парсинга JSON
+class ParsingError : public std::runtime_error {
+public:
+    using runtime_error::runtime_error;
+};
+
+class Node final : std::variant<std::nullptr_t, Array, Dict, bool, int, double, std::string> {
+public:
+    using variant::variant;
+    using Value = variant;
+    
+    explicit Node(std::string_view sv) : value_(std::string(std::move(sv))) {}
+    
+    inline const Value& GetValue() const { return *this; }
+    
+    bool operator==(const Node& rhs) const;
+    bool operator!=(const Node& rhs) const;
+    
+    bool IsInt() const;
+    bool IsPureDouble() const;
+    bool IsDouble() const;
+    bool IsBool() const;
+    bool IsString() const;
+    bool IsNull() const;
+    bool IsArray() const;
+    bool IsMap() const;
+    
+    int AsInt() const;
+    double AsDouble() const;
+    bool AsBool() const;
+    const std::string& AsString() const;
+    const Array& AsArray() const;
+    const Dict& AsMap() const;
+    
+private:
+    Value value_;
+};
+
+class Document {
+public:
+    explicit Document(Node root) : root_(std::move(root)) {}
+    
+    inline const Node& GetRoot() const { return root_; }
+    
+    bool operator==(const Document& rhs) const;
+    bool operator!=(const Document& rhs) const;
+    
+private:
+    Node root_;
+};
+
+Document Load(std::istream& input);
+
+void Print(const Document& doc, std::ostream& output, int step, int indent);
+
+}  // namespace json
