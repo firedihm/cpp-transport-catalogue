270ef412caf865e8895a0144d3bf97715e0ae851
firedihm <42544757+firedihm@users.noreply.github.com>
2024-06-15 15:23:56 +0300

    Create ranges.h

---
+++ transport-catalogue/ranges.h
@@ -0,0 +1,25 @@
+#pragma once
+
+namespace ranges {
+
+template <typename It>
+class Range {
+public:
+    using ValueType = typename std::iterator_traits<It>::value_type;
+    
+    Range(It begin, It end) : begin_(begin) , end_(end) {}
+    
+    inline It begin() const { return begin_; }
+    inline It end() const { return end_; }
+    
+private:
+    It begin_;
+    It end_;
+};
+
+template <typename C>
+auto AsRange(const C& container) {
+    return Range{container.begin(), container.end()};
+}
+
+}  // namespace ranges
