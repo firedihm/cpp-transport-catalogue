5f5795391aa2e50b7f7572f5925c939ccc390788
firedihm <42544757+firedihm@users.noreply.github.com>
2024-06-16 23:12:44 +0300

    Update json_reader.h

--- transport-catalogue/json_reader.h
+++ transport-catalogue/json_reader.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "json.h"
-#include "map_renderer.h"
 #include "transport_catalogue.h"
 
 namespace json {
@@ -12,20 +11,19 @@ public:
         : catalogue_(catalogue), input_(input), output_(output) {}
     
     void ProcessBaseRequests();
-    const Document ProcessStatRequests();
     void PrintStats(int step = 4, int indent = 0);
-    void RenderMap(/* int step = 0, int indent = 4 */);
+    void RenderMap(int step = 0, int indent = 4);
     
 private:
     Dict MakeBusResponse(const Dict& request);
     Dict MakeStopResponse(const Dict& request);
-    Dict MakeMapResponse(const Dict& request);
     
-    static geo::Coordinates ParseCoordinates(const Dict& request);
-    static std::vector<std::pair<std::string_view, int>> ParseDistances(const Dict& request);
-    static std::vector<std::string_view> ParseRoute(const Dict& request);
-    static svg::Color ParseColor(const Node& node);
-    static render::RenderDetails ParseRenderDetails(const Dict& settings);
+    /* методы ниже опираются на вспомогательные объекты, которые лениво инициализируются в ProcessStatRequests()
+     * static Dict MakeMapResponse(const Dict& request, const MapRenderer& renderer);
+     * static Dict MakeRouteResponse(const Dict& request, const TransportRouter& router);
+     */
+    
+    const Document ProcessStatRequests();
     
     catalogue::TransportCatalogue& catalogue_;
     const Document& input_;
