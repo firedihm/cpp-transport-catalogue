efe456f5ab33ed9ff34b0d397a6638b763aa4bb6
firedihm <42544757+firedihm@users.noreply.github.com>
2024-06-17 21:54:53 +0300

    Update input_reader.cpp

--- transport-catalogue/input_reader.cpp
+++ transport-catalogue/input_reader.cpp
@@ -1,157 +1,253 @@
-#include "input_reader.h"
-#include "transport_catalogue.h"
+#include "json_builder.h"
+#include "json_reader.h"
+#include "map_renderer.h"
+#include "transport_router.h"
 
-#include <algorithm>
-#include <cassert>
-#include <iterator>
+#include <sstream>
 
-namespace catalogue::input {
+using namespace std::literals;
+using namespace catalogue;
+
+namespace json {
+
+using MapRenderer = std::unique_ptr<render::MapRenderer>;
+using TransportRouter = std::unique_ptr<router::TransportRouter>;
+
 namespace detail {
-/*
- * Парсит строку вида "10.123,  -30.1837" и возвращает пару координат (широта, долгота)
- */
-geo::Coordinates ParseCoordinates(std::string_view str) {
-    static const double nan = std::nan("");
-
-    auto not_space = str.find_first_not_of(' ');
-    auto comma = str.find(',', not_space);
-    if (comma == str.npos) {
-        return {nan, nan};
-    }
-
-    auto not_space2 = str.find_first_not_of(' ', comma + 1);
-    auto comma2 = str.find(',', not_space2); // может быть npos
-
-    double lat = std::stod(std::string(str.substr(not_space, str.find_last_not_of(' ', comma - 1) + 1 - not_space)));
-    double lng = std::stod(std::string(str.substr(not_space2, str.find_last_not_of(' ', comma2 - 1) + 1 - not_space2)));
-
-    return {lat, lng};
+geo::Coordinates ParseCoordinates(const Dict& request) {
+    return { request.at("latitude"s).AsDouble(), request.at("longitude"s).AsDouble() };
 }
 
-std::vector<std::pair<std::string_view, int>> ParseDistances(std::string_view str) {
-    using namespace std::string_literals;
+std::vector<std::pair<std::string_view, int>> ParseDistances(const Dict& request) {
+    const Dict& data = request.at("road_distances"s).AsMap();
+    
     std::vector<std::pair<std::string_view, int>> result;
-    
-    // пропускаем первые две запятые, которые отделяют координаты остановки
-    auto comma = str.find(',');
-    comma = str.find(',', comma + 1);
-    
-    while (comma != str.npos) {
-        auto not_space = str.find_first_not_of(' ', comma + 1);
-        auto m = str.find('m', not_space);
-        
-        auto not_space2 = str.find("to"s, m);
-        not_space2 = str.find_first_not_of(' ', not_space2 + 2);
-        comma = str.find(',', comma + 1); // может быть npos
-        
-        result.emplace_back(str.substr(not_space2, str.find_last_not_of(' ', comma - 1) + 1 - not_space2),
-                            std::stoi(std::string(str.substr(not_space, m - not_space))));
+    result.reserve(data.size());
+    for (const auto& /* std::string, Node */ [destination, distance] : data) {
+        result.emplace_back(destination, distance.AsInt());
     }
-    
     return result;
 }
 
-/*
- * Удаляет пробелы в начале и конце строки
- */
-std::string_view Trim(std::string_view string) {
-    const auto start = string.find_first_not_of(' ');
-    if (start == string.npos) {
-        return {};
-    }
-    return string.substr(start, string.find_last_not_of(' ') + 1 - start);
-}
-
-/*
- * Разбивает строку string на n строк, с помощью указанного символа-разделителя delim
- */
-std::vector<std::string_view> Split(std::string_view string, char delim) {
+std::vector<std::string_view> ParseRoute(const Dict& request) {
+    const Array& data = request.at("stops"s).AsArray();
+    
     std::vector<std::string_view> result;
-
-    size_t pos = 0;
-    while ((pos = string.find_first_not_of(' ', pos)) < string.length()) {
-        auto delim_pos = string.find(delim, pos);
-        if (delim_pos == string.npos) {
-            delim_pos = string.size();
-        }
-        if (auto substr = Trim(string.substr(pos, delim_pos - pos)); !substr.empty()) {
-            result.push_back(substr);
-        }
-        pos = delim_pos + 1;
+    result.reserve(data.size());
+    for (const Node& entry : data) {
+        result.emplace_back(entry.AsString());
     }
-
     return result;
 }
 
-/*
- * Парсит маршрут.
- * Для кольцевого маршрута (A>B>C>A) возвращает массив названий остановок [A,B,C,A]
- * Для некольцевого маршрута (A-B-C-D) возвращает массив названий остановок [A,B,C,D,C,B,A]
- */
-std::vector<std::string_view> ParseRoute(std::string_view route) {
-    if (route.find('>') != route.npos) {
-        return Split(route, '>');
+svg::Color ParseColor(const Node& node) {
+    if (node.IsString()) {
+        return node.AsString();
     }
-
-    auto stops = Split(route, '-');
-    std::vector<std::string_view> results(stops.begin(), stops.end());
-    results.insert(results.end(), std::next(stops.rbegin()), stops.rend());
-
-    return results;
+    
+    const Array& color = node.AsArray();
+    if (color.size() == 3) {
+        return svg::Rgb(color[0].AsInt(), color[1].AsInt(), color[2].AsInt());
+    } else {
+        return svg::Rgba(color[0].AsInt(), color[1].AsInt(), color[2].AsInt(), color[3].AsDouble());
+    }
 }
 
-CommandDescription ParseCommandDescription(std::string_view line) {
-    size_t colon = line.find(':');
-    if (colon == line.npos) {
-        return {};
+render::RenderSettings ParseRenderSettings(const Dict& settings) {
+    render::RenderSettings result;
+    
+    result.width = settings.at("width"s).AsDouble();
+    result.height = settings.at("height"s).AsDouble();
+    result.padding = settings.at("padding"s).AsDouble();
+    
+    result.line_width = settings.at("line_width"s).AsDouble();
+    result.stop_radius = settings.at("stop_radius"s).AsDouble();
+    
+    result.bus_label_font_size = settings.at("bus_label_font_size"s).AsInt();
+    result.bus_label_offset = svg::Point(settings.at("bus_label_offset"s).AsArray()[0].AsDouble(),
+                                          settings.at("bus_label_offset"s).AsArray()[1].AsDouble());
+    
+    result.stop_label_font_size = settings.at("stop_label_font_size"s).AsInt();
+    result.stop_label_offset = svg::Point(settings.at("stop_label_offset"s).AsArray()[0].AsDouble(),
+                                           settings.at("stop_label_offset"s).AsArray()[1].AsDouble());
+    
+    result.underlayer_color = ParseColor(settings.at("underlayer_color"s));
+    result.underlayer_width = settings.at("underlayer_width"s).AsDouble();
+    
+    const Array& palette = settings.at("color_palette"s).AsArray();
+    std::vector<svg::Color> colors;
+    colors.reserve(palette.size());
+    for (const Node& color : palette) {
+        colors.push_back(ParseColor(color));
     }
+    result.colors = std::move(colors);
+    
+    return result;
+}
 
-    size_t delim = line.find(' ', line.find_first_not_of(' '));
-    if (delim >= colon) {
-        return {};
-    }
-
-    size_t not_space = line.find_first_not_of(' ', delim);
-    if (not_space >= colon) {
-        return {};
-    }
-
-    return {std::string(line.substr(line.find_first_not_of(' '), delim)),
-            std::string(line.substr(not_space, line.find_last_not_of(' ', colon - 1) + 1 - not_space)),
-            std::string(line.substr(colon + 1))};
+router::RoutingSettings ParseRouteSettings(const Dict& settings) {
+    return { settings.at("bus_wait_time"s).AsInt(), settings.at("bus_velocity"s).AsDouble() };
 }
 } // namespace detail
 
-void InputReader::ParseLine(std::string_view line) {
-    auto command_description = detail::ParseCommandDescription(line);
-    if (command_description) {
-        commands_.push_back(std::move(command_description));
-    }
-}
-
-void InputReader::ApplyCommands(TransportCatalogue& catalogue) const {
-    using namespace std::literals;
+void JsonReader::ProcessBaseRequests() {
+    const Array& requests = input_.GetRoot().AsMap().at("base_requests"s).AsArray();
+    
+    // запомним отложенные запросы, чтобы не итерировать по всему массиву на каждом этапе
+    std::vector<uint> distances, buses;
+    distances.reserve(requests.size());
+    buses.reserve(requests.size());
     
     // сначала инициализируем остановки...
-    for (const auto& command : commands_) {
-        if (command.command == "Stop"s) {
-            catalogue.AddStop(command.id, detail::ParseCoordinates(command.description));
+    for (uint id = 0; id < requests.size(); ++id) {
+        const Dict& request = requests[id].AsMap();
+        std::string_view type = request.at("type"s).AsString();
+        
+        if (type == "Stop"sv) {
+            catalogue_.AddStop(request.at("name"s).AsString(), detail::ParseCoordinates(request));
+            if (!request.at("road_distances"s).AsMap().empty()) {
+                distances.push_back(id);
+            }
+        } else if (type == "Bus"sv) {
+            buses.push_back(id);
         }
     }
     
     // ...затем расстояния между ними...
-    for (const auto& command : commands_) {
-        if (command.command == "Stop"s) {
-            catalogue.AddDistance(command.id, detail::ParseDistances(command.description));
+    for (uint id : distances) {
+        const Dict& request = requests[id].AsMap();
+        for (const auto& /* <std::pair<std::string_view, int>> */ [destination, distance] : detail::ParseDistances(request)) {
+            catalogue_.AddDistance(request.at("name"s).AsString(), destination, distance);
         }
     }
     
     // ...потом маршруты
-    for (const auto& command : commands_) {
-        if (command.command == "Bus"s) {
-            catalogue.AddBus(command.id, detail::ParseRoute(command.description));
-        }
+    for (uint id : buses) {
+        const Dict& request = requests[id].AsMap();
+        catalogue_.AddBus(request.at("name"s).AsString(), detail::ParseRoute(request), request.at("is_roundtrip"s).AsBool());
     }
 }
 
-} // namespace catalogue::input
+void JsonReader::PrintStats(int step, int indent) {
+    json::Print(ProcessStatRequests(), output_, step, indent);
+}
+
+void JsonReader::RenderMap(int step, int indent) {
+    render::MapRenderer i(detail::ParseRenderSettings(input_.GetRoot().AsMap().at("render_settings"s).AsMap()), catalogue_);
+    i.RenderMap(output_, step, indent);
+}
+
+Dict JsonReader::MakeBusResponse(const Dict& request) {
+    Dict response;
+    if (const Bus* bus = catalogue_.GetBus(request.at("name"s).AsString())) {
+        double route_length = static_cast<double>(catalogue_.CalculateRouteLength(bus));
+        response["request_id"s] = request.at("id"s).AsInt();
+        response["stop_count"s] = static_cast<int>(bus->route.size());
+        response["unique_stop_count"s] = catalogue_.CountUniqueStops(bus);
+        response["route_length"s] = route_length;
+        response["curvature"s] = route_length / catalogue_.CalculateRouteGeoLength(bus);
+    } else {
+        response["request_id"s] = request.at("id"s).AsInt();
+        response["error_message"s] = "not found"s;
+    }
+    return response;
+}
+
+Dict JsonReader::MakeStopResponse(const Dict& request) {
+    Dict response;
+    if (const Stop* stop = catalogue_.GetStop(request.at("name"s).AsString())) {
+        const std::set<std::string_view>* buses = catalogue_.GetBusesForStop(stop);
+        
+        //Array routes(buses->begin(), buses->end()); // не работает -- БРЕД
+        Array routes;
+        for (auto it = buses->begin(); it != buses->end(); ++it) {
+            routes.emplace_back(std::string(*it));
+        }
+        response["request_id"s] = request.at("id"s).AsInt();
+        response["buses"s] = std::move(routes);
+    } else {
+        response["request_id"s] = request.at("id"s).AsInt();
+        response["error_message"s] = "not found"s;
+    }
+    return response;
+}
+
+Dict MakeMapResponse(const Dict& request, const MapRenderer& renderer) {
+    std::ostringstream oss;
+    renderer->RenderMap(oss, 0, 4);
+    
+    Dict response;
+    response["request_id"s] = request.at("id"s).AsInt();
+    response["map"s] = std::move(*oss.rdbuf()).str();
+    return response;
+}
+
+Dict MakeRouteResponse(const Dict& request, const TransportRouter& router) {
+    //router::RoutingSettings = ;
+    
+    return {request};
+}
+
+const Document JsonReader::ProcessStatRequests() {
+    const Array& requests = input_.GetRoot().AsMap().at("stat_requests"s).AsArray();
+    
+    // вспомогательные объекты будут инициализироваться только если поступит соответствующий запрос
+    MapRenderer renderer(nullptr);
+    TransportRouter router(nullptr);
+    
+    Array response;
+    response.reserve(requests.size());
+    for (const Node& request : requests) {
+        std::string_view type = request.AsMap().at("type"s).AsString();
+        if (type == "Bus"sv) {
+            response.push_back(MakeBusResponse(request.AsMap()));
+        } else if (type == "Stop"sv) {
+            response.push_back(MakeStopResponse(request.AsMap()));
+        } else if (type == "Map"sv) {
+            if (!renderer.get()) {
+                const Dict& settings = input_.GetRoot().AsMap().at("render_settings"s).AsMap();
+                renderer = std::make_unique<render::MapRenderer>(detail::ParseRenderSettings(settings),
+                                                                 catalogue_);
+            }
+            response.push_back(MakeMapResponse(request.AsMap(), renderer));
+        } else if (type == "Route"sv) {
+            if (!router.get()) {
+                const Dict& settings = input_.GetRoot().AsMap().at("route_settings"s).AsMap();
+                router = std::make_unique<router::TransportRouter>(detail::ParseRouteSettings(settings),
+                                                                   catalogue_);
+            }
+            response.push_back(MakeRouteResponse(request.AsMap(), router));
+        }
+    }
+    return Document(Node(response));
+    
+    /* нельзя объявить ctx как Builder::ArrayContext потому что он приватный, но как auto можно -- гениально
+    Builder builder = json::Builder();
+    auto ctx = builder.StartArray();
+    for (const Node& request : requests) {
+        std::string_view type = request.AsMap().at("type"s).AsString();
+        if (type == "Bus"sv) {
+            ctx.Value(MakeBusResponse(request.AsMap()));
+        } else if (type == "Stop"sv) {
+            ctx.Value(MakeStopResponse(request.AsMap()));
+        } else if (type == "Map"sv) {
+            if (!renderer.get()) {
+                const Dict& settings = input_.GetRoot().AsMap().at("render_settings"s).AsMap();
+                renderer = std::make_unique<render::MapRenderer>(detail::ParseRenderSettings(settings),
+                                                                 catalogue_);
+            }
+            ctx.Value(MakeMapResponse(request.AsMap(), renderer));
+        } else if (type== "Route"sv) {
+            if (!router.get()) {
+                const Dict& settings = input_.GetRoot().AsMap().at("route_settings"s).AsMap();
+                router = std::make_unique<router::TransportRouter>(detail::ParseRouteSettings(settings),
+                                                                   catalogue_);
+            }
+            ctx.Value(MakeRouteResponse(request.AsMap(), router));
+        }
+    }
+    return Document(ctx.EndArray().Build());
+    */
+}
+
+} // namespace json
