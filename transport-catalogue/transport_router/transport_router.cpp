ed1bad9ea44aeb969687c16a3faf55bef5fbef2d
firedihm <42544757+firedihm@users.noreply.github.com>
2024-06-17 21:54:17 +0300

    Update transport_router.cpp

--- transport-catalogue/transport_router/transport_router.cpp
+++ transport-catalogue/transport_router/transport_router.cpp
@@ -1,13 +1,17 @@
 #include "transport_router.h"
 
+using namespace catalogue;
+
 namespace router {
 
-void TransportRouter::InitGraphStopEdges(const std::deque<catalogue::Stop>& stops) {
+void TransportRouter::InitGraphStopEdges() {
+    const std::deque<Stop>& stops = catalogue_.GetStopsData();
+    
     stop_to_vertices_.reserve(stops.size());
     edge_to_response_.reserve(stops.size());
     
     Weight wait_time = static_cast<Weight>(settings_.wait_time);
-    for (graph::VertexId index = 0; const catalogue::Stop& stop : stops) {
+    for (graph::VertexId index = 0; const Stop& stop : stops) {
         graph::Edge<Weight> edge = {index, index + 1, wait_time};
         
         graph_.AddEdge(edge);
@@ -20,8 +24,18 @@ void TransportRouter::InitGraphStopEdges(const std::deque<catalogue::Stop>& stop
     }
 }
 
-void TransportRouter::InitGraphBusEdges(const std::deque<catalogue::Bus>& buses) {
-    
+void TransportRouter::InitGraphBusEdges() {
+    for (const catalogue::Bus& bus : catalogue_.GetBusesData()) {
+        double travel_time = 0.0;
+        for (auto from = bus.route.begin(), to = from + 1; to != bus.route.end(); ++from, ++to) {
+            travel_time += catalogue_.GetDistanceBetweenStops(*from, *to) / settings_.velocity;
+            
+            
+            
+            graph::Edge<Weight> edge = {index, index + 1, wait_time};
+            graph_.AddEdge(edge);
+        }
+    }
 }
 
 } // namespace router
