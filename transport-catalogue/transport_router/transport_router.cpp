88ad491e38c6888439fea64bf484e917e9c4fdfb
firedihm <42544757+firedihm@users.noreply.github.com>
2024-06-18 21:54:01 +0300

    Update transport_router.cpp

--- transport-catalogue/transport_router/transport_router.cpp
+++ transport-catalogue/transport_router/transport_router.cpp
@@ -28,7 +28,7 @@ void TransportRouter::InitGraphBusEdges() {
     for (const catalogue::Bus& bus : catalogue_.GetBusesData()) {
         double travel_time = 0.0;
         
-        struct TempShortestTime { const Stop* from, to; int span; double time; };
+        struct TempShortestTime { const Stop* from, * to; int span; double time; };
         std::vector<TempShortestTime> shortest_times;
         shortest_times.reserve(bus.route.size());
         
@@ -41,15 +41,14 @@ void TransportRouter::InitGraphBusEdges() {
              */
             if (auto it = std::find_if(shortest_times.begin(), shortest_times.end(),
                                        [from, to](const TempShortestTime& elem) {
-                                           return *from == elem.from && *to == elem.to;
+                                           return (*from == elem.from) && (*to == elem.to);
                                        }); it == shortest_times.end()) {
                 shortest_times.emplace_back(*from, *to, std::distance(from, to), travel_time);
             }
             
             for (const TempShortestTime& shortest_time : shortest_times) {
-                graph::VertexId from_i = stop_to_vertices_[shortest_time.from->name];
-                graph::VertexId to_i = stop_to_vertices_[shortest_time.to->name];
-                graph::Edge<Weight> edge = {from_i, to_i, shortest_time.time};
+                StopVertices vx = stop_to_vertices_[shortest_time.from->name];
+                graph::Edge<Weight> edge = {vx.from, vx.to, shortest_time.time};
                 
                 graph_.AddEdge(edge);
                 
@@ -59,9 +58,9 @@ void TransportRouter::InitGraphBusEdges() {
     }
 }
 
-std::pair<Weight, std::vector<ResponseItem>> TransportRouter::BuildRoute(std::string_view from,
-                                                                            std::string_view to) const {
-    std::optional<graph::Router<Weight>::RouteInfo> result();
+std::pair<TransportRouter::Weight, std::vector<ResponseItem>> TransportRouter::BuildRoute(std::string_view from,
+                                                                                          std::string_view to) const {
+    std::optional<graph::Router<Weight>::RouteInfo> result;
 }
 
 } // namespace router
