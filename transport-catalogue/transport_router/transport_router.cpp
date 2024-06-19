1b1a8c8fea60a4a7339cf2a72824fcd8cb02aa6f
firedihm <42544757+firedihm@users.noreply.github.com>
2024-06-19 19:30:08 +0300

    Update transport_router.cpp

--- transport-catalogue/transport_router/transport_router.cpp
+++ transport-catalogue/transport_router/transport_router.cpp
@@ -4,14 +4,11 @@ using namespace catalogue;
 
 namespace router {
 
-void TransportRouter::InitGraphStopEdges() {
-    const std::deque<Stop>& stops = catalogue_.GetStopsData();
-    
-    stop_to_vertices_.reserve(stops.size());
-    edge_to_response_.reserve(stops.size());
-    
+void TransportRouter::InitGraphWaitEdges() {
+    stop_to_vertices_.reserve(catalogue_.GetStopsData().size());
     Weight wait_time = static_cast<Weight>(settings_.wait_time);
-    for (graph::VertexId index = 0; const Stop& stop : stops) {
+    
+    for (graph::VertexId index = 0; const Stop& stop : catalogue_.GetStopsData()) {
         graph::Edge<Weight> edge{index, index + 1, wait_time};
         
         graph_.AddEdge(edge);
@@ -26,36 +23,38 @@ void TransportRouter::InitGraphStopEdges() {
 
 void TransportRouter::InitGraphBusEdges() {
     for (const catalogue::Bus& bus : catalogue_.GetBusesData()) {
-        double travel_time = 0.0;
-        
         /*
          * Если автобус проезжает между некоторыми остановками несколько раз, 
-         * то храним наименьшее время пути на этом отрезке, т.е. оставляем первую 
-         * запись для отрезка, потому что travel_time растёт с каждой итерацией. 
-         * В структуре будем хранить уже обработанные отрезки.
+         * то храним наименьшее время пути на этом отрезке.
          */
-        struct ProcessedSpan { const Stop* from, * to; };
-        std::vector<ProcessedSpan> processed_spans;
-        processed_spans.reserve(bus.route.size());
+        struct Record { double time; int span; };
+        std::unordered_map<std::pair<const Stop*, const Stop*>, Record, TransportCatalogue::StopPtrsHasher> span_to_time;
         
-        for (auto from = bus.route.begin(), to = from + 1; to != bus.route.end(); ++from, ++to) {
-            travel_time += catalogue_.GetDistanceBetweenStops(*from, *to) / settings_.velocity;
+        // оценим все возможные отрезки на маршруте автобуса
+        for (auto from = bus.route.begin(); from != bus.route.end() - 1; ++from) {
+            Weight travel_time = 0.0;
             
-            // если текущий отрезок ещё не повторялся...
-            if (auto it = std::find_if(processed_spans.begin(), processed_spans.end(),
-                                       [from, to](const ProcessedSpan& span) {
-                                           return (*from == span.from) && (*to == span.to);
-                                       }); it == processed_spans.end()) {
-                processed_spans.emplace_back(*from, *to);
+            for (auto current = from, to = from + 1; to != bus.route.end(); ++current, ++to) {
+                travel_time += catalogue_.GetDistanceBetweenStops(*current, *to) / settings_.velocity;
                 
-                graph::Edge<Weight> edge{stop_to_vertices_[(*from)->name].end,
-                                         stop_to_vertices_[(*to)->name].begin,
-                                         travel_time};
+                std::pair<const Stop*, const Stop*> key{*from, *to};
+                Record value{travel_time, static_cast<int>(std::distance(from, to))};
+                if (auto it = span_to_time.find(key); it == span_to_time.end()) {
+                    span_to_time[key] = value;
+                } else if (travel_time < it->second.time) {
+                    it->second = value;
+                }
+            }
+            
+            for (const auto& [span, record] : span_to_time) {
+                graph::Edge<Weight> edge{stop_to_vertices_[span.first->name].end,
+                                         stop_to_vertices_[span.second->name].begin,
+                                         record.time};
                 
                 graph_.AddEdge(edge);
                 
                 // добавим данные во вспомогательные объекты
-                edge_to_response_.emplace(edge, BusResponse(bus.name, std::distance(from, to), travel_time));
+                edge_to_response_.emplace(edge, BusResponse(bus.name, record.span, record.time));
             }
         }
     }
@@ -64,7 +63,7 @@ void TransportRouter::InitGraphBusEdges() {
 std::optional<TransportRouter::RouteResponse> TransportRouter::BuildRoute(std::string_view from,
                                                                           std::string_view to) const {
     std::optional<RouteResponse> result(std::nullopt);
-    if (auto route = router_.BuildRoute(stop_to_vertices_.at(from).begin, stop_to_vertices_.at(to).begin)) {
+    if (auto route = router_->BuildRoute(stop_to_vertices_.at(from).begin, stop_to_vertices_.at(to).begin)) {
         std::vector<ResponseItem> response_items;
         for (graph::EdgeId edge_id : route->edges) {
             response_items.push_back(edge_to_response_.at(graph_.GetEdge(edge_id)));
